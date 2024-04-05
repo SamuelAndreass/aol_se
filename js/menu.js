969b26743820a47d169b95c2fe6a64a1aea1808a
SamuelAndreass <rudibego775@gmail.com>
2024-04-05 22:02:43 +0700

    upd

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+const navbarNav = document.querySelector('.navbar-nav');
+//humberger box clicked
+document.querySelector('#hamburger-box').onclick = () => {
+    navbarNav.classList.toggle('active');
+}
+
+const hamburger = document.querySelector('#hamburger-box');
