0dff4571f16aa6b1dbb3998bae7d28416a538b1b
SamuelAndreass <rudibego775@gmail.com>
2024-04-06 13:45:49 +0700

    coba2

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,26 @@
-const navbarNav = document.querySelector('.navbar-nav');
-//humberger box clicked
-document.querySelector('#hamburger-box').onclick = () => {
-    navbarNav.classList.toggle('active');
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("sign-btn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
 }
 
-const hamburger = document.querySelector('#hamburger-box');
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
