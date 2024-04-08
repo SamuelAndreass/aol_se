c0d4e5f36549cbcf32d4a0e70da4043fbde56a22
SamuelAndreass <rudibego775@gmail.com>
2024-04-08 12:04:35 +0700

    login php

---
+++ html/config.php
@@ -0,0 +1,3 @@
+<?php
+    $con = mysqli_connect("localhost","root","", "aol_se");
+?>
--- html/login..php
+++ html/login..php
@@ -41,8 +41,37 @@
   
  <div class="regis-wrapper" style= "background-image: url(../asset/bg-registration-tenant.svg);";>
   <div class="form-login">
+    <?php
+    include ("config.php");
+    if (isset($_POST['submit'])) {
+      $nama2 = isset($_POST['nama2']) ? $_POST['nama2'] : '';
+      $phonenum2 = isset($_POST['phonenum2']) ? $_POST['phonenum2'] : '';
+      $mail = isset($_POST['mail']) ? $_POST['mail'] : '';
+      $password = isset($_POST['password']) ? $_POST['password'] : '';
+    
+      $verif_query = mysqli_query($con, "SELECT userEmail FROM users WHERE userEmail = '$mail'");
+
+      if (!$verif_query) {
+        die('Error: ' . mysqli_error($con));
+    }
+
+      if (mysqli_num_rows($verif_query) != 0) {
+        echo "<div class message> 
+                <p> email already in use <p>
+                <div><br>";
+        echo "<a href = javascript:self.history.back()'><button class='btn'> Go back";          
+    }else {
+      mysqli_query($con,"INSERT INTO users(userName, userPhone, userEmail, userPassword) VALUES('$nama2', '$phonenum2', '$mail', '$password')");
+
+      echo "<div class message> 
+                <p> Registration success <p>
+                <div><br>";
+        echo "<a href = <a href='index.php'> <button class='btn'>login now </button>";
+    }
+  } else{  
+    ?>
     <h1 class="login-title">Daftar Akun</h1>
-    <form action="../php/register.php" method="POST">
+    <form action="" method="POST">
       <label for="name">Nama lengkap</label><br>
       <input type="text" name="nama2" placeholder="Masukkan nama lengkap anda"><br>
       <label for="phone">Nomor telpon</label><br>
@@ -55,11 +84,13 @@
       <input type="password" name="r-password" placeholder="Minimal 8 karakter"><br>
 
       <div class="button-container">
-        <input type="submit"id="button-login">Daftar</br>
+        <input type="submit"name="submit" value="Daftar" id="button-login"></button>
+
       </div>
       
     </form>
   </div>
+  <?php } ?>
  </div>
       
  
