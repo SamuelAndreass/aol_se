450f4d18bb7760ccacd206e1e58fa08450d4db08
RivannoHR <rivannohaidarrozaan@gmail.com>
2024-05-26 22:47:02 +0700

    Nyoba dynamic page :V

--- page/menu.php
+++ page/menu.php
@@ -218,7 +218,49 @@
     <!-- login penyewa -->
     <!-- template header --> 
       <script src="../js/menu.js"></script>
+        <!--Dynamic Content start-->
+      <div class="container-xxl py-5">
+          <div class="tab-content">
+              <div id="tab-1" class="tab-pane fade show p-0 active">
+                  <div class="row g-4">
+                    <?php 
+                        include ('config.php');
+                          $queryKos = "SELECT * FROM kos";
+                          $getKos = mysqli_query($con,$queryKos);
+                            if (mysqli_num_rows($getKos) > 0){
+                                while($row = mysqli_fetch_assoc($getKos)){
+                                  $kostId = $row['idKos'];
+                                  ?>
+                                  <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                                    <div class="property-item rounded overflow-hidden">
+                                      <div class="position-relative overflow-hidden">
+                                        <a href=""><img class="img-fluid" src=<?php echo '../images/' . $row['gambarKos'];?> alt=""></a>
+                                        <!-- <div class="bg-primary rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3">For Rent</div> Ini buat tag di gambar -->
+                                        <div class="bg-white rounded-top text-primary position-absolute start-0 bottom-0 mx-4 pt-1 px-3"><?php echo $row['namaKos'];?></div> <!--Sama keknya bagus buat-->
+                                      </div>
+                                      <div class="p-4 pb-0">
+                                        <h5 class="text-primary mb-3"><?php echo $row['hargaKos'];?></h5>
+                                        <p><i class="fa fa-map-marker-alt text-primary me-2"></i><?php echo $row['alamatKos'];?></p>
+                                      </div>
+                                    </div>
+                                  </div>
 
+                                <?php } 
+                            }
+                      ?>
+
+
+
+                  
+                    <div class="col-12 text-center wow fadeInUp" data-wow-delay="0.1s">
+                                <a class="btn btn-primary py-3 px-5" href="">Liat kos lainnya</a>
+                            
+                    </div>
+                    </div>
+               </div>
+            </div>
+      </div>
+        <!-- Dynamic Content end -->  
       
 </body>
 
